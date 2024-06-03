e10d9e9ef23dace96265f1ee89b46d4e7d3d69f4
Mohamadrez@ <m.reza.golbaba@gmail.com>
2024-06-03 23:32:06 +0300

    initial code

---
+++ 1/HelloWorld.rb
@@ -0,0 +1,12 @@
+ puts "Hello, World!"
+ p "Hello, World!"
+ print "Hello, World!"
+
+ greeting = "Hello, World!"
+ puts greeting
+
+def sya_hello (things_to_say)
+    puts things_to_say
+end
+
+sya_hello "Hello, World from a function!"
